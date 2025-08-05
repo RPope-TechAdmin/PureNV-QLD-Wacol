06fb0602c100d58096d5981cc7941265725512e3
RPope-TechAdmin <rpope@purenv.au>
2025-08-05 17:05:54 +1000

    Modifying return to display response from backend

--- src/upload-lab-script.js
+++ src/upload-lab-script.js
@@ -97,7 +97,7 @@ if (selectedFile.size > 4 * 1024 * 1024) {
     console.log("🚨 Server error:", data);
     output.textContent = `Upload failed: ${data?.error || "Unknown error"}`;
   } else {
-    warningDiv.textContent = "Data uploaded successfully, Thank you!";
+    warningDiv.textContent = response;
   }
 } catch (err) {
   console.log("🚨 Upload error:", err.message);
